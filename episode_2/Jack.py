971465bb7e49b8dd84e577f46bc2a2d7244ef86c
Jack Mitcheltree <60791537+ChucklesJM@users.noreply.github.com>
2020-08-25 13:51:32 -0700

    Update Jack.py

--- episode_2/Jack.py
+++ episode_2/Jack.py
@@ -1,20 +1,16 @@
 #Find the sum of the digits in the number 100!
-def factorial(n):
-    L = list(range(1,n))
-    v = 1
-    for x in range(n-1):
-        v = v * L[x]
-    return v
+import time
+import math
 
-def make_digits(n):
-    digits = []
-    temp = n
-    while temp > 0:
-        hold = temp % 10
-        digits.append(hold)
-        temp = temp - hold
-        temp = temp // 10
-    digits.reverse()
-    return digits
+start = time.time()
 
-print(sum(make_digits(factorial(100))))
+def F(n):
+    F = str(math.prod(list(range(1,n+1))))
+    S = 0
+    for x in range(len(F)):
+        S = int(F[x]) + S
+    return S
+print(F(100))
+
+end = time.time()
+print(end-start)
