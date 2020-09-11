660c7a7288ac7362480f7e60a2040017db17ced3
Clarisse <60796422+clarguerwill@users.noreply.github.com>
2020-09-11 14:41:38 -0400

    Update Clarisse.py

--- episode_5/Clarisse.py
+++ episode_5/Clarisse.py
@@ -1,5 +1,4 @@
-n = 200
-ways = [1] + [0]*n
+ways = [1] + [0]*200
 for coin in [1, 2, 5, 10, 20, 50, 100, 200]:
-  for i in range(coin, n+1): ways[i] += ways[i-coin]
-print (ways[n])
+  for i in range(coin, len(ways)): ways[i] += ways[i-coin]
+print (ways[-1])
