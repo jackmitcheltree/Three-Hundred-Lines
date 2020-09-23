5eb3e14099b2350750da57daa9319a730d176662
Clarisse <60796422+clarguerwill@users.noreply.github.com>
2020-09-23 12:58:43 -0400

    Update Clarisse.py

--- episode_6/Clarisse.py
+++ episode_6/Clarisse.py
@@ -6,16 +6,16 @@ start = time.time()
 import math as m
 n = 10000
 primes = [True if i > 1 else False for i in range(n+1)] 
-for p in range(2, n+1):
-  if p >= 9 and p%2 == 1 and primes[p] is not True:
-    a = [False, p]
-    for j in range(3, p):
-      if primes[j] is True and m.sqrt((p-j)/2).is_integer():
-        a = [True, p]
+for i in range(2, n+1):
+  if i >= 9 and i%2 == 1 and primes[i] is not True:
+    a = [False, i]
+    for j in range(3, i):
+      if primes[j] is True and m.sqrt((i-j)/2).is_integer():
+        a = [True, i]
         break
-  if p >= 9 and a[0] is False: break
-  if p*p <= n and primes[p] is True: 
-    for i in range(p * p, n+1, p): primes[i] = False
+  if i >= 9 and a[0] is False: break
+  if i*i <= n and primes[i] is True: 
+    for p in range(i*i, n+1, i): primes[p] = False
 print (a)
 
 
