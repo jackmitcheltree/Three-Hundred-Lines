03229fff5205cd8efa9ed2f29459493f7d94eded
Clarisse <60796422+clarguerwill@users.noreply.github.com>
2020-10-03 17:33:40 -0400

    Update Clarisse.py

--- episode_6/Clarisse.py
+++ episode_6/Clarisse.py
@@ -9,7 +9,7 @@ for i in range(2, n+1):
   if i >= 9 and i%2 == 1 and primes[i] is not True:
     a = [False, i]
     for j in range(3, i):
-      if primes[j] is True and ((i-j)/2)^.5.is_integer():
+      if primes[j] is True and (((i-j)/2)**.5).is_integer():
         a = [True, i]
         break
   if i >= 9 and a[0] is False: break
