c3b4b5669196a81f7609a34186c6ecbd7c2eff0a
Clarisse <60796422+clarguerwill@users.noreply.github.com>
2020-10-24 11:04:30 -0400

    Add files via upload

---
+++ e29.py
@@ -0,0 +1,16 @@
+import time
+start = time.time()
+
+
+
+import numpy as p
+if (x:=100+1*(y:=2) >= 0: print(len(p.unique([(i)**(j) for i in range(y, x) for j in range(y, x)])))
+
+
+
+end = time.time()
+print(end-start)
+
+
+
+
